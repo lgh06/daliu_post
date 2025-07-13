adae25d048c7975f2d6adabd78036de6f50f0709
hnnk <i@passby.me>
2025-07-13 16:51:49 +0800

    头条

--- packages/back/auto/providers/index.mjs
+++ packages/back/auto/providers/index.mjs
@@ -1,2 +1,3 @@
 export * from "./weixin_dingyue_personal.mjs"
 export * from "./x_personal.mjs"
+export * from "./toutiao_personal.mjs"
---
+++ packages/back/auto/providers/toutiao_personal.mjs
@@ -0,0 +1,69 @@
+// 头条 mp 个人号
+import puppeteer from 'puppeteer-core';
+import { autoCommons } from '../autoCommons.mjs'
+
+let { wait } = autoCommons;
+
+
+
+async function main({title="111",content="222"}) {
+  let browser = await puppeteer.launch({
+    ...autoCommons.basicLauchOptions
+  });
+  /**
+   * @type {import('puppeteer-core').Page}
+   */
+  let page = await browser.newPage();
+
+  try {
+
+    // 打开链接
+    await page.goto('https://mp.toutiao.com/',{waitUntil:'domcontentloaded'});
+
+
+    // 等待 文本输入框 出现
+    await page.waitForSelector('#masterRoot div.pgc-content div.byte-menu-inline.base_creation_tab div.byte-menu-item:nth-child(1) a',{timeout:0});
+    await wait()
+    // 点击 文本输入框
+    await page.click('#masterRoot div.pgc-content div.byte-menu-inline.base_creation_tab div.byte-menu-item:nth-child(1) a');
+
+    await page.waitForSelector('.publish-editor .assistant-title .title-wrapper .publish-editor-title .editor-title')
+    await wait()
+    await page.click('.publish-editor .assistant-title .title-wrapper .publish-editor-title .editor-title')
+    await wait()
+    await page.keyboard.sendCharacter(title)
+    await wait()
+    await page.waitForSelector('.publish-editor .syl-editor .ProseMirror p')
+    await page.click('.publish-editor .syl-editor .ProseMirror p')
+    await wait()
+    await page.keyboard.sendCharacter(content)
+
+
+
+    console.log("执行完毕 你需要自己点击 保存为草稿 按钮。")
+
+
+
+
+  } catch (error) {
+    console.error(error)
+  } finally {
+    // await browser.close();
+    // process.exit(0);
+  }
+}
+
+// main({
+//   content: "你好，我是程序员 这里是内容 测试测试",
+// })
+
+let toutiao_personal = {
+  main,
+  path: "toutiao_personal",
+  desc: "头条-个人",
+  params: "title,content"
+}
+
+
+export { toutiao_personal }
+
