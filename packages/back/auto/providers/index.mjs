badd812fa97f9432062754c6fea71bb0539498da
hnnk <i@passby.me>
2025-07-21 16:31:50 +0800

    微信订阅号 个人 纯文字

--- packages/back/auto/providers/index.mjs
+++ packages/back/auto/providers/index.mjs
@@ -3,5 +3,6 @@ export * from './toutiao_personal_micro.mjs';
 export * from './weibo_xinxianshi_personal.mjs';
 export * from './weixin_dingyue_personal.mjs';
 export * from './weixin_dingyue_personal2.mjs';
+export * from './weixin_dingyue_personal_text.mjs';
 export * from './x_personal_corplink.mjs';
 export * from './x_personal_socks5.mjs';
