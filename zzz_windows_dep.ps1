1756dab77535c593aa3721e7503e146503bfdc2e
hnnk <i@passby.me>
2025-07-25 08:38:37 +0800

    upd

--- zzz_windows_dep.ps1
+++ zzz_windows_dep.ps1
@@ -1,3 +1,4 @@
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser
 Invoke-WebRequest -Uri "https://cnb.cool/lgh06/daliu_post_dependencies/-/lfs/47fe1d46dbb7111f6693b04a8bd95fc869ce2062df7b4822b52849548fb457e4?name=Git-2.50.1-64-bit.exe" -OutFile "Git-2.50.1-64-bit.exe"  
 .\"Git-2.50.1-64-bit.exe" /SILENT /VERYSILENT --silent --install
 Start-Sleep -Seconds 30
