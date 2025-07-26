4a44dd3f5cf6a086fc96d619e322a589d905f7a4
hnnk <i@passby.me>
2025-07-26 21:14:36 +0800

    add basic server sent events

--- packages/back/server.mjs
+++ packages/back/server.mjs
@@ -54,6 +54,28 @@ app.get('/providers', async (req, res) => {
   res.json(result);
 });
 
+// https://masteringjs.io/tutorials/express/server-sent-events
+app.get('/progress', async (req, res) => {
+    console.log('Got /progress');
+    res.set({
+      'Cache-Control': 'no-cache',
+      'Content-Type': 'text/event-stream',
+      'Connection': 'keep-alive'
+    });
+    res.flushHeaders();
+
+    // Tell the client to retry every 10 seconds if connectivity is lost
+    res.write('retry: 10000\n\n');
+    let count = 0;
+
+    while (true) {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      console.log('Emit', ++count);
+      // Emit an SSE that contains the current 'count' as a string
+      res.write(`data: ${count}\n\n`);
+    }
+});
+
 await import('./auto/providers/index.mjs').then((providers) => {
 
   Object.keys(providers).forEach((providerName) => {
